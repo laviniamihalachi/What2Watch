90654c8e8d7db23f31533f21d7b9410b93dfe8e5
anma12 <anamaria_melinte2000@yahoo.com>
2019-04-03 19:26:59 +0300

    refresh frontend

--- What2WatchFrontend/src/app/layouts/admin-layout/admin-layout.module.ts
+++ What2WatchFrontend/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -6,10 +6,10 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
+import { TypographyComponent } from '../../theme/typography/typography.component';
+import { IconsComponent } from '../../theme/icons/icons.component';
+import { MapsComponent } from '../../theme/maps/maps.component';
+import { NotificationsComponent } from '../../theme/notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
 
 import {
--- What2WatchFrontend/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ What2WatchFrontend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,10 +3,10 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
+import { TypographyComponent } from '../../theme/typography/typography.component';
+import { IconsComponent } from '../../theme/icons/icons.component';
+import { MapsComponent } from '../../theme/maps/maps.component';
+import { NotificationsComponent } from '../../theme/notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
 
 export const AdminLayoutRoutes: Routes = [
