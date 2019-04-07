85f9696fbcf64b327fa17d5af4a801be208f31dc
laviniamihalachi <34279768+laviniamihalachi@users.noreply.github.com>
2019-04-07 11:07:42 -0700

    profile &movie conttroller

--- What2Watch/Controllers/ProfileController.cs
+++ What2Watch/Controllers/ProfileController.cs
@@ -13,11 +13,17 @@ namespace What2Watch.Controllers
 
         private ApplicationDbContext db = new ApplicationDbContext();
 
+        //Get the current user profile
         public ActionResult Index()
         {
+            var currentUserId = User.Identity.GetUserId();
+            Profile profile = db.Profiles.Where(i => i.User.Id == currentUserId).FirstOrDefault();
+            ViewBag.Profile = profile;
             return View();
         }
 
+
+        // create profile - get method
         public ActionResult New()
         {
             var currentUserId = User.Identity.GetUserId();
@@ -43,6 +49,46 @@ namespace What2Watch.Controllers
             }
         }
 
+        //Show a specific profile 
+        public ActionResult Show(int id)
+        {
+            Profile profile = db.Profiles.Find(id);
+            ViewBag.Profile = profile;
 
+            return View();
+        }
+
+        //getMethod for editing the profile
+        public ActionResult Edit(int id)
+        {
+            Profile profile = db.Profiles.Find(id);
+            ViewBag.Profile = profile;
+            return View();
+        }
+
+        [HttpPut]
+        public ActionResult Edit(int id, Profile requestProfile)
+        {
+            try
+            {
+                Profile profile = db.Profiles.Find(id);
+                if (TryUpdateModel(profile))
+                {
+                    //TempData["message"] = "S-a modificat " + profile.FirstName;
+                    profile.FirstName = requestProfile.FirstName;
+                    profile.LastName = requestProfile.LastName;
+                    profile.Description = requestProfile.Description;
+                    profile.Photo = requestProfile.Photo;
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+                }
+                //TempData["message"] = "NU s-a modificat " + profile.ProfileId + id;
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+        }
     }
 }
